a2b012f9e77efa2cd3fb271057c0eb8fa1250ff3
Your Name <nova2005jag@gmail.com>
2026-08-02 12:41:13 -0300

    problema del escroll del modal solucionado

--- src/components/cardP/ModalProject.jsx
+++ src/components/cardP/ModalProject.jsx
@@ -13,12 +13,18 @@ export default function ModalProject({
   useEffect(() => {
     if (open) {
       document.body.style.overflow = "hidden";
+      document.body.style.position = "fixed";
+      document.body.style.width = "100%";
     } else {
-      document.body.style.overflow = "auto";
+      document.body.style.overflow = "";
+      document.body.style.position = "";
+      document.body.style.width = "";
     }
 
     return () => {
-      document.body.style.overflow = "auto";
+      document.body.style.overflow = "";
+      document.body.style.position = "";
+      document.body.style.width = "";
     };
   }, [open]);
 
